d68fd86309bc0cb50d2d315ac53dfe5cf7c36690
Kaho Cheng <chengy1989@hotmail.com>
2022-11-22 17:40:25 +0900

    policies update

--- app/policies/subscription_policy.rb
+++ app/policies/subscription_policy.rb
@@ -5,4 +5,28 @@ class SubscriptionPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    true
+  end
 end
