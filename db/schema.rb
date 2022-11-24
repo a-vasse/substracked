ccfd6f28317e91f02bd9ea64af2c7881a4cb3c3a
Kaho Cheng <chengy1989@hotmail.com>
2022-11-24 13:29:36 +0900

    linebot set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_031726) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_032618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_031726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.text "logo"
+    t.string "logo"
     t.index ["user_id"], name: "index_resources_on_user_id"
   end
 
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_031726) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "line_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-User.create(name: "Winston", email: "user@me.com", password: "test123")
-
+user = User.find_by(email: "user@me.com")
+if user
+  user.line_id="kazuhide89"
+  user.save
+else
+  User.create(name: "Winston", email: "user@me.com", password: "test123", line_id: "kazuhide89")
+end
 require_relative "seeds/resources_seeds"
 require_relative "seeds/plans_seeds"
 require_relative "seeds/subscriptions_seeds"
