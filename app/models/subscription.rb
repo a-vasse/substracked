12cbafb9fb39d62778e0d71348627aae30c3f542
Patrick Stark <contact@patrick-stark.com>
2022-11-28 10:12:09 +0900

    added dougs code for upcoming subscriptions to model and controller

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,6 +6,22 @@ class Subscription < ApplicationRecord
   validates :renewal_date, presence: true
   validates :start_date, presence: true
 
+  scope :upcoming_yearly,
+        -> {
+          joins(:plan).where(
+            "EXTRACT(DOY FROM renewal_date)::int BETWEEN ? AND ? AND status = ? AND plans.billing_cycle = ?",
+            *[Date.today.yday, 1.week.from_now.yday, true, 12],
+          )
+        }
+  scope :upcoming_monthly,
+        -> {
+          joins(:plan).where(
+            "EXTRACT(DAY FROM renewal_date) IN (?) AND status = ? AND plans.billing_cycle = ?",
+            *[(Date.today..1.week.from_now).to_a.map(&:day), true, 1],
+          )
+        }
+  scope :upcoming, -> { upcoming_monthly.or(upcoming_yearly) }
+
   def display_name
     "#{plan.resource.name} - #{region}"
   end
