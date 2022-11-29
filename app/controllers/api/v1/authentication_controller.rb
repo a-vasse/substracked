f0d514a7bc5d948ddde2f20c80303cad776e0731
Alex V <alexandervasse@gmail.com>
2022-11-29 13:30:19 +0900

    authenticate fix

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AuthenticationController < Api::V1::BaseController
-  skip_before_action :authenticate_request
+  # skip_before_action :authenticate_request
 
   def login
     skip_authorization
