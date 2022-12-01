1bcb9f0c3800c422ed5993a69f30d3d8c28e3678
Patrick Stark <contact@patrick-stark.com>
2022-12-01 18:18:07 +0900

    combined kaho and mine work

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -2,27 +2,79 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="notifications"
 export default class extends Controller {
-  static values = {
-    id: Number
-  };
+  static targets = ["resource", "plans", "form", "custom", "add", "toast"];
 
   connect() {
-    console.log("Notifications connected")
-    this.element.addEventListener('hide.bs.toast', () => {
-      const url = `/api/v1/notifications/${this.idValue}`
-      const data = {
-        notification: {
-          read: true
-        }
-      }
+    // console.log("Notifications connected")
+    if (this.hasToastTarget) {
+      this.toastTarget.addEventListener('hide.bs.toast', () => {
+        this.markAsRead();
+      })
+    }
+  }
 
-      fetch(url, {
-        method: 'PATCH',
-        headers:  {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }).then(response => console.log(response));
-    })
+  markAsRead() {
+    const url = `/api/v1/notifications/${this.toastTarget.dataset.notificationId}`
+    const data = {
+      notification: {
+        read: true
+      }
+    }
+
+    fetch(url, {
+      method: 'PATCH',
+      headers:  {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  }
+  fetchForm() {
+    console.log(this.resourceTarget.value);
+    this.resourceTarget.value = "Amazon Prime";
+    this.display();
+  }
+
+  display() {
+    this.toastTarget.classList.remove("show");
+    this.markAsRead();
+
+    const resource = this.resourceTarget.value;
+    if (resource != "") {
+      fetch("/api/v1/resources")
+        .then((response) => response.json())
+        .then((dataResources) => {
+          let targetResourceID = dataResources.find(
+            (dataResource) => dataResource.name === resource
+          ).id;
+          fetch("/api/v1/plans")
+            .then((response) => response.json())
+            .then((dataPlans) => {
+              this.plansTarget.innerHTML = "";
+              let targetPlans = dataPlans.filter(
+                (dataPlan) => dataPlan.resource_id === targetResourceID
+              );
+              let selectOptions = `<div class="mb-3 select required subscription_plan">
+                <label class="form-label select required" for="subscription_plan_id">Plan <abbr title="required">*</abbr></label>
+                <select class="form-select select required" name="subscription[plan_id]" id="subscription_plan_id">`;
+              if (targetPlans[0].name === "") {
+                selectOptions += `<option value="${targetPlans[0].id}">N/A</option>`;
+              } else {
+                targetPlans.forEach((plan) => {
+                  selectOptions += `<option value="${plan.id}">${plan.name} - ¥ ${plan.price}</option>`;
+                });
+              }
+              selectOptions += "</div>";
+              this.plansTarget.insertAdjacentHTML("afterbegin", selectOptions);
+              this.formTarget.classList.remove("d-none");
+              this.addTarget.classList.remove("d-none");
+              this.customTarget.classList.add("d-none");
+            });
+        });
+    } else {
+      this.formTarget.classList.add("d-none");
+      this.addTarget.classList.add("d-none");
+      this.customTarget.classList.remove("d-none");
+    }
   }
 }
